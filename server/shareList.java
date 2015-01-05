85d6a212b1ba417bae2bc39e94d801797f7381be
Kata Hazi <cfdrws@users.noreply.github.com>
2015-01-05 11:50:47 +0800

    Create new file

---
+++ server/shareList.java
@@ -0,0 +1,16 @@
+Enter file contents here
+package minet;
+import java.net.Socket;
+import java.util.List;
+
+
+//date struct -share date
+class shareList {
+	String miroSocketInfList; // 在线用户信息列表，形式：username + ";" + miroIP + ";" + port + ";"
+	static List<Socket> socketList;// socket list
+
+	shareList(String m, List<Socket> s) {
+		miroSocketInfList = m;
+		socketList = s;
+	}
+}
